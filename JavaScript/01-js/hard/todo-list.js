9b7e7b30e98139bfddc377b4fe9ef0e4583c35c4
Fayaz139 <123862731+Fayaz139@users.noreply.github.com>
2026-02-02 22:38:49 +0530

    add Todo class with methods for managing a todo list

---
+++ JavaScript/01-js/hard/todo-list.js
@@ -0,0 +1,51 @@
+/*
+  Implement a class `Todo` having below methods
+    - add(todo): adds todo to list of todos
+    - remove(indexOfTodo): remove todo from list of todos
+    - update(index, updatedTodo): update todo at given index
+    - getAll: returns all todos
+    - get(indexOfTodo): returns todo at given index
+    - clear: deletes all todos
+
+  Once you've implemented the logic, test your code by running
+  - `npm run test-todo-list`
+*/
+
+class Todo {
+  constructor() {
+    this.todo = [];
+  }
+
+  add(str) {
+    this.todo.push(str);
+  }
+
+  remove(index) {
+    this.todo.splice(index, 1);
+  }
+
+  update(index, str) {
+    if (index <= this.todo.length-1) {
+      this.todo.splice(index, 1, str);
+    }
+  }
+
+  getAll() {
+    return this.todo;
+  }
+
+  get(n) {
+    if (n <= this.todo.length-1) {
+      return this.todo[n];
+    }
+    else {
+      return null
+    }
+  }
+
+  clear() {
+    this.todo = [];
+  }
+}
+
+module.exports = Todo;

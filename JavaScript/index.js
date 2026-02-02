c21c21ec07d89af6c126ae391d6a054d39593253
Fayaz139 <123862731+Fayaz139@users.noreply.github.com>
2026-02-02 16:09:27 +0530

    Practice

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,8 +1,24 @@
 const fs = require("fs");
 
-function fileReadCallback(err, contents) {
-    console.log(contents);
-    console.log(contents);
-    console.log(contents);
+// function fileReadCallback(err, contents) {
+//     console.log(contents);
+//     console.log(contents);
+//     console.log(contents);
+// }
+
+
+// fs.readFile("a.txt", "utf-8", function fileReadCallback(err, contents) {
+//     console.log(contents);
+    
+// });
+
+// let sum = 0;
+// for (let i = 0; i <= 5; i++) {
+//     sum += i;
+// }
+let time = 0;
+function timer() {
+    console.log(time);
+    time = time+1;
 }
-fs.readFile("a.txt", "utf-8", fileReadCallback);
+

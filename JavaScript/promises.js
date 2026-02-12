daf92e9b2b24fb423d87bb7b56ef665d8abe2d86
Fayaz139 <123862731+Fayaz139@users.noreply.github.com>
2026-02-12 19:47:43 +0530

    Starting Promises

---
+++ JavaScript/promises.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+function fsReadFilePromisified(filePath, encoding) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, encoding, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        })
+    })
+}
